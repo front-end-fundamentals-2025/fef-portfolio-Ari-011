71f0ffaf7e8ac0596a7d6560236bc826b8688300
Ari-011 <alar24rn@student.ju.se>
2025-02-17 21:51:52 +0100

    wrapper-constant-js
    a wrapper constant was added in the eventListeners for the contact form container to change its color

--- js/eventlistener.js
+++ js/eventlistener.js
@@ -3,17 +3,27 @@
 const dark = document.getElementById('dark-button');
 const light = document.getElementById('light-button');
 
-
 dark.addEventListener("click", darkMode);
 light.addEventListener("click", lightMode);
 
 
 function darkMode(){
-  document.body.style.background = "black";
-  document.body.style.color = "white";
+
+  document.body.style.background = 'black';
+  document.body.style.color = 'white';
+
+  const wrapper = document.getElementsByClassName('wrapper')[0];
+  wrapper.style.backgroundColor = '#808080';
+  wrapper.style.color = 'white';
+  
 }
 
 function lightMode(){
-  document.body.style.background = "White";
-  document.body.style.color = "Black";
+  document.body.style.background = 'White';
+  document.body.style.color = 'Black';
+
+  const wrapper = document.getElementsByClassName('wrapper')[0];
+  wrapper.style.backgroundColor = 'white';
+  wrapper.style.color = 'black';
+
 }
